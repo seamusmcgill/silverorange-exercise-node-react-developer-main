b87b6a87c23023be63dcaa22c0e96afbaa571d4e
Seamus McGill <seamusmcgill@gmail.com>
2022-05-11 13:28:06 -0400

    Reads from repos.json file and adds valid repos to array

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -6,3 +6,11 @@ const checkIfValidRepo = (repository) => {
     reposArray.push(repository);
   }
 };
+
+  // Read from repos.json file
+  fs.readFile(path.resolve('data/repos.json')).then((data) => {
+    // Append repos that match the fork condition into the array of repos
+    data.forEach((repository) => {
+      checkIfValidRepo(repository);
+    });
+  });
