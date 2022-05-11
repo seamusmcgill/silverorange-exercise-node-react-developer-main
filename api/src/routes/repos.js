5728e99cc8e6a24371157d1dadf1174624f2fd3e
Seamus McGill <seamusmcgill@gmail.com>
2022-05-11 13:27:43 -0400

    Creates function to check if repo should be returned

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -1,1 +1,8 @@
 const reposArray = [];
+
+// Function to append valid repos to reposArray
+const checkIfValidRepo = (repository) => {
+  if (!repository.fork) {
+    reposArray.push(repository);
+  }
+};
