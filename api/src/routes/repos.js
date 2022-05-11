6a3a527be0809af4c8bb8d1dad49291cc8125953
Seamus McGill <seamusmcgill@gmail.com>
2022-05-11 19:54:41 -0400

    Adds error catching

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -14,30 +14,37 @@ const checkIfValidRepo = (repository, reposArray) => {
 
 repos.get('/', async (_, res) => {
   // Read from repos.json file
-  fs.readFile(path.resolve('data/repos.json')).then((data) => {
-    // Initialize array of repos to be returned
-    const reposArray = [];
-    // Append repos that match the fork condition into the array of repos
-    data.forEach((repository) => {
-      checkIfValidRepo(repository, reposArray);
-    });
-    axios
-      // Fetch repos data from GitHub API
-      .get('https://api.github.com/users/silverorange/repos')
-      .then((response) => {
-        const githubRepos = response.data;
-        // Append repos that match the fork condition into the array of repos
-        githubRepos.forEach((repository) => {
-          checkIfValidRepo(repository, reposArray);
-        });
-        // Set response header content-type to application/json
-        res.header({
-          'Cache-Control': 'no-store',
-          'Content-Type': 'application/json',
-        });
-        res.status(200);
-        // Return the array of valid repos
-        res.json(reposArray);
+  fs.readFile(path.resolve('data/repos.json'))
+    .then((data) => {
+      // Initialize array of repos to be returned
+      const reposArray = [];
+      // Append repos that match the fork condition into the array of repos
+      data.forEach((repository) => {
+        checkIfValidRepo(repository, reposArray);
       });
-  });
+      axios
+        // Fetch repos data from GitHub API
+        .get('https://api.github.com/users/silverorange/repos')
+        .then((response) => {
+          const githubRepos = response.data;
+          // Append repos that match the fork condition into the array of repos
+          githubRepos.forEach((repository) => {
+            checkIfValidRepo(repository, reposArray);
+          });
+          // Set response header content-type to application/json
+          res.header({
+            'Cache-Control': 'no-store',
+            'Content-Type': 'application/json',
+          });
+          res.status(200);
+          // Return the array of valid repos
+          res.json(reposArray);
+        })
+        .catch((err) => {
+          throw new Error(err.message);
+        });
+    })
+    .catch((err) => {
+      throw new Error(err.message);
+    });
 });
