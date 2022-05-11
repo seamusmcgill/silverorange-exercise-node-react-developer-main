96d3fbfd32672c716d0caf634df52994422babda
Seamus McGill <seamusmcgill@gmail.com>
2022-05-11 15:33:59 -0400

    Refactors to append array asynchronously

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -5,11 +5,8 @@ import axios from 'axios';
 
 export const repos = Router();
 
-// Initialize array of repos to be returned
-const reposArray = [];
-
 // Function to append valid repos to reposArray
-const checkIfValidRepo = (repository) => {
+const checkIfValidRepo = (repository, reposArray) => {
   if (repository.fork === false) {
     reposArray.push(repository);
   }
@@ -18,30 +15,29 @@ const checkIfValidRepo = (repository) => {
 repos.get('/', async (_, res) => {
   // Read from repos.json file
   fs.readFile(path.resolve('data/repos.json')).then((data) => {
+    // Initialize array of repos to be returned
+    const reposArray = [];
     // Append repos that match the fork condition into the array of repos
     data.forEach((repository) => {
-      checkIfValidRepo(repository);
+      checkIfValidRepo(repository, reposArray);
     });
-  });
-
-  // Fetch repos data from GitHub API
-  axios
-    .get('https://api.github.com/users/silverorange/repos')
-    .then((response) => {
-      const githubRepos = response.data;
-      // Append repos that match the fork condition into the array of repos
-      githubRepos.forEach((repository) => {
-        checkIfValidRepo(repository);
+    axios
+      // Fetch repos data from GitHub API
+      .get('https://api.github.com/users/silverorange/repos')
+      .then((response) => {
+        const githubRepos = response.data;
+        // Append repos that match the fork condition into the array of repos
+        githubRepos.forEach((repository) => {
+          checkIfValidRepo(repository, reposArray);
+        });
+        // Set response header content-type to application/json
+        res.header({
+          'Cache-Control': 'no-store',
+          'Content-Type': 'application/json',
+        });
+        res.status(200);
+        // Return the array of valid repos
+        res.json(reposArray);
       });
-    });
-  // Set response header content-type to application/json
-  res.header({
-    'Cache-Control': 'no-store',
-    'Content-Type': 'application/json',
   });
-
-  res.status(200);
-
-  // Return the array of valid repos
-  res.json(reposArray);
 });
